64feb884c115ec8a49db074c8964ab3dc1084d0a
Armağan Dalkıran <armagandalkiran@Armagan-MacBook-Pro.local>
2022-07-08 00:18:25 +0300

    todoapp-task-3 interface bug fixed

--- src/models/constants.ts
+++ src/models/constants.ts
@@ -1,13 +1,6 @@
-<<<<<<< HEAD
-export const navbarItems = {
-    title:"My ToDo list",
-    description:"welcome to todolist with typescript and jestu"
-}
-=======
 import { NavbarItems } from "./interfaces";
 
-export const navbarItems: NavbarItems = {
-  title: "My ToDo list",
-  description: "lorem ipsum lorem ipsum lorem ipsum",
-};
->>>>>>> 4705ecb (todoapp-task-1 navbar tests added)
+export const navbarItems:NavbarItems = {
+    title:"My ToDo list",
+    description:"welcome to todolist with typescript and jest"
+}
