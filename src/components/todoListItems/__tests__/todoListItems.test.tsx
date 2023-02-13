761845845af9be97f81b048cbb9f9bc2054096f5
Armagan Dalkiran <77741597+armagandalkiran@users.noreply.github.com>
2023-02-13 10:48:47 +0300

    Update todoListItems.test.tsx

--- src/components/todoListItems/__tests__/todoListItems.test.tsx
+++ src/components/todoListItems/__tests__/todoListItems.test.tsx
@@ -4,21 +4,6 @@ import renderer from "react-test-renderer";
 import { ToDo } from "../../todo";
 
 describe("TodoList tests", () => {
-  // test("Todolist component listing", () => {
-  //   render(<ToDo />);
-  //   const checkbox = screen.getAllByRole("checkbox");
-
-  //   // const defaultTasks = screen.getAllByText(/^default/);
-  //   // expect(defaultTasks.length).toEqual(3);
-  //   // expect(checkbox.length).toEqual(3);
-  //   // expect(deleteButton.length).toEqual(3);
-  // });
-
-  // it("should test handlecheckbox method",()=>{
-  //   const setStateMock = jest.fn();
-  //   const useStateMock:any = (useState:any) => [useState,setStateMock];
-  //   jest.spyOn(React,"useState").mockImplementation(useStateMock);
-  // })
 
   test("matches snapshot", () => {
     const tree = renderer.create(<ToDo />).toJSON();
